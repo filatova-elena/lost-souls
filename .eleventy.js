b86069a51ef3e982ea356460bdbb4c90e9dfd6e3
Elena Filatova <elena.vl.filatova@gmail.com>
2026-02-05 21:03:06 -0800

    Fix duplicate permalink and asset passthrough paths

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function(eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/quests/*.css");
   eleventyConfig.addPassthroughCopy("src/clues/*.css");
