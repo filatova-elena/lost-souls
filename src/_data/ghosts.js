624a79566875ba2e390b0b4b02a1fc43fb7c27b7
Elena Filatova <elenafilatova@elenas-mbp.lan>
2026-02-17 14:49:57 -0800

    Added scripts

---
+++ src/_data/ghosts.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const loadYamlDir = require('../utils/loadYamlDir');
+
+// When Eleventy loads this file, __dirname points to src/_data
+const ghostsDir = path.join(__dirname, 'ghosts');
+const ghosts = loadYamlDir(ghostsDir, false);
+
+// Create a map by ghost id for easy lookup
+const ghostsById = {};
+ghosts.forEach(ghost => {
+  if (ghost.id) {
+    ghostsById[ghost.id] = ghost;
+  }
+  if (ghost.ghost) {
+    ghostsById[ghost.ghost] = ghost;
+  }
+});
+
+module.exports = {
+  all: ghosts,
+  byId: ghostsById
+};
